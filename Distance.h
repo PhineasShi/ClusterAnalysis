b6eb7924c5350a972125f2107a3e720a04ffd9b0
建杰 史 <519430889@qq.com>
2016-11-29 20:35:15 +0800

    Did some comments in Chinese.

--- Distance.h
+++ Distance.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <math.h>
-typedef enum DistType
+typedef enum DistType			//距离类型枚举
 {
-	euclidean,
-	manhattan,
-	chebyshev,
-	minkowski,
-	measure,
+	euclidean,		//欧式距离
+	manhattan,		//绝对值距离
+	chebyshev,		//切氏距离
+	minkowski,		//闵氏距离
+	measure,		//匹配测度
 };
 class Distance
 {
@@ -15,16 +15,17 @@ public:
 	Distance(int dim, double *vec1, double *vec2,DistType distType) :dim(dim),vec1(vec1),vec2(vec2),distType(distType) {};
 	~Distance();
 
-	double getDist();
+	double getDist();		//计算并返回距离
 
 private:
-	double dist;
-	int dim;
-	double *vec1;
+	double dist;			//计算得到的距离
+	int dim;				//向量的维度
+	double *vec1;			//需要计算距离的两个向量
 	double *vec2;
 	DistType distType;		//距离类型
 	int m;					//闵氏距离中的参数
 
+	//计算各种距离函数
 	void calEuDist();
 	void calMaDist();
 	void calChDist();
